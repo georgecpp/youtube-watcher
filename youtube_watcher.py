5646a3cf17befe0fa0377d341c0abd0265896ed0
George Andronache <georgeandronache.cpp@gmail.com>
2024-01-15 21:25:15 +0100

    fetching specific video data

--- youtube_watcher.py
+++ youtube_watcher.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
+# source env/bin/activate to activate
 
 import json
 import logging
+from pprint import pformat
 import sys
 import requests
 from config import config
@@ -11,6 +13,17 @@ def fetch_playlist_items_page(google_api_key, youtube_playlist_id, page_token=No
         "key":google_api_key,
         "playlistId": youtube_playlist_id,
         "part": "contentDetails",
+        "pageToken": page_token
+    })
+    payload = json.loads(response.text)
+    logging.debug("GOT %s", payload)
+    return payload
+
+def fetch_videos_page(google_api_key, video_id, page_token=None):
+    response = requests.get("https://www.googleapis.com/youtube/v3/videos",params={
+        "key":google_api_key,
+        "id": video_id,
+        "part": "snippet,statistics",
         "pageToken": page_token
     })
     payload = json.loads(response.text)
@@ -27,12 +40,33 @@ def fetch_playlist_items(google_api_key, youtube_playlist_id, page_token=None):
     if next_page_token is not None:
         yield from fetch_playlist_items(google_api_key,youtube_playlist_id,next_page_token)
 
+def fetch_videos(google_api_key, youtube_playlist_id, page_token=None):
+    # fetch one page
+    payload = fetch_videos_page(google_api_key, youtube_playlist_id, page_token)
+    # serve up items from that page
+    yield from payload["items"]
+    next_page_token = payload.get("nextPageToken")
+    # if there is another page, carry on from there.    
+    if next_page_token is not None:
+        yield from fetch_videos(google_api_key,youtube_playlist_id,next_page_token)
+
+def summarize_video(video):
+    return {
+        "video_id": video["id"],
+        "title": video["snippet"]["title"],
+        "views": int(video["statistics"].get("viewCount",0)),
+        "likes": int(video["statistics"].get("likeCount",0)),
+        "comments": int(video["statistics"].get("commentCount",0)),
+    }
+
 def main():
     logging.info("START")
     google_api_key = config["google_api_key"]
     youtube_playlist_id = config["youtube_playlist_id"]
     for video_item in fetch_playlist_items(google_api_key,youtube_playlist_id):
-        logging.info("GOT %s", video_item)
+        video_id = video_item["contentDetails"]["videoId"]
+        for video in fetch_videos(google_api_key, video_id):
+            logging.info("GOT %s", pformat(summarize_video(video)))
 
 
 if __name__ == "__main__":
