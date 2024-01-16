d7b5ba79ec49150aaff2e3fdf9dcedc0f34e0bf6
George Andronache <georgeandronache.cpp@gmail.com>
2024-01-16 20:42:10 +0100

    serializing data with avro and streaming to kafka.

--- config.py
+++ config.py
@@ -1,4 +1,15 @@
 config={
     "google_api_key":"AIzaSyBy4kO21sekxbG4xO7srj-TXRGquLC2cpg",
     "youtube_playlist_id": "PLUihDZDRJtI5Pgnn00xaoxG7b6hwnBRiY",
+    "kafka": {
+        "bootstrap.servers": "pkc-l7q2j.europe-north1.gcp.confluent.cloud:9092",
+        "security.protocol": "sasl_ssl",
+        "sasl.mechanism": "PLAIN",
+        "sasl.username": "QPFUCMJTQVFTWBXT",
+        "sasl.password": "MJh7/HUFzqn4hxgLEOA6v7FwC+9boK4MhZ6Ayf0F+S7sSZXpAsscseI5KGsR1CSv",
+    },
+    "schema_registry": {
+        "url": "https://psrc-9zg5y.europe-west3.gcp.confluent.cloud",
+        "basic.auth.user.info": "WJX7WSBHEFNDJIW3:3d3wU7ebb/Ef4VbhHVzWQmNjAaMnAKSov6R5YQp8M9oBE8LBawvxf0epfSpQVE/a",
+    }
 }
